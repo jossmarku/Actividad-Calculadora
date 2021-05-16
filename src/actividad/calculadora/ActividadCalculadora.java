fdbb2b53e581165b9f54d7d5bcdbc4d92bae44d6
Personal <Personal@Familia>
2021-05-16 18:47:01 -0500

    Documentación de la Actividad Calculadora

--- src/actividad/calculadora/ActividadCalculadora.java
+++ src/actividad/calculadora/ActividadCalculadora.java
@@ -2,16 +2,31 @@ package actividad.calculadora;
 
 import java.util.Scanner;
 
+/**
+ * Proyecto que incluye una calculadora funcional de dos valores en Java
+ * 
+ * @author Jossmar Oswaldo Ku Cupul
+ * @author Universidad Autónoma de Campeche
+ * @since Mayo 2021
+ * @version 1.0
+ * 
+ */
 public class ActividadCalculadora {
 
+    /**
+     * Este método imprime el encabezado de nuestro proyecto
+     */
     public static void encabezado(){
         System.out.println("Universidad Autónoma de Campeche");
         System.out.println("Facultad de Ingeniería");
         System.out.println("Ingeniería en Sistemas Computacionales");
-        System.out.println("11 de mayo de 2021");
+        System.out.println("16 de mayo de 2021");
         System.out.println("Jossmar Oswaldo Ku Cupul");
     }
     
+    /**
+     * Este método imprime el cuerpo de nuestro proyecto
+     */
     public static void cuerpo() {
         System.out.println("---------------------------------------------------------------------");
         System.out.println("");
@@ -19,12 +34,19 @@ public class ActividadCalculadora {
         System.out.println("");
     }
     
+    /**
+     * Este método imprime el pie de nuestro proyecto
+     */
     public static void pie() {
         System.out.println("");
         System.out.println("=====================================================================");
         System.out.println("                                                            JOKC.2021");
     }
     
+    /**
+     * Este método le pide al usuario dos valores, y posteriormente le pide que escoja una operación a realizar entre ambos valores
+     * Una vez escogida la operación, mediante un switch se detecta cuál fue la operación, y se imprimen los valores calculados en la clase Operaciones
+     */
     public static void principal() {
         int operacion;
         Scanner entrada = new Scanner(System.in);
@@ -75,6 +97,10 @@ public class ActividadCalculadora {
         }
     }
     
+    /**
+     * En el método main se ejecutan los cuatro métodos anteriores
+     * @param args 
+     */
     public static void main(String[] args) {
         encabezado();
         cuerpo();
--- src/actividad/calculadora/Operaciones.java
+++ src/actividad/calculadora/Operaciones.java
@@ -4,27 +4,52 @@ public class Operaciones {
     
     double primerValor, segundoValor;
 
+    /**
+     * Este método trasnfiere los dos valores introducidos por el usuario para utilizarlos en las operaciones establecidas
+     * @param x Es el primer valor introducido por el usuario
+     * @param y Es el segundo valor introducido por el usuario
+     */
     public void setValores(double x, double y) {
         this.primerValor = x;
         this.segundoValor = y;
     }
 
+    /**
+     * Este método calcula la suma entre los dos valores introducidos por el usuario
+     * @return Devuelve el valor de la suma en tipo double
+     */
     public double Suma() {
         return primerValor + segundoValor;
     }
 
+    /**
+     * Este método calcula la resta entre los dos valores introducidos por el usuario
+     * @return Devuelve el valor de la resta en tipo double
+     */
     public double Resta() {
         return primerValor - segundoValor;
     }
 
+    /**
+     * Este método calcula la multiplicación entre los dos valores introducidos por el usuario
+     * @return Devuelve el valor de la multiplicación en tipo double
+     */
     public double Multiplicacion() {
         return primerValor * segundoValor;
     }
 
+    /**
+     * Este método calcula la división entre los dos valores introducidos por el usuario
+     * @return Devuelve el valor de la división en tipo double
+     */
     public Double Division() {
       return primerValor / segundoValor;
     }
     
+    /**
+     * Este método imprime un mensaje de advertencia cuando el usuario pretenda dividir entre cero
+     * @return Devuelve un mensaje de advertencia en tipo String cuando el usuario intente dividir entre cero
+     */
     public String ErrorDivision() {
         return "No puede dividir entre cero.";
     }
